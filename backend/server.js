574812db89da29dc50624268c7a5c1aa0d95fdb9
wes-public-apps <murraywj@yahoo.com>
2021-01-29 07:28:09 -0800

    successful connect server to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
+/* This file defines the server for the todos application. */
+
+//Import Libraries
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+//Define Constants
 const PORT = 4000;
 
 app.use(cors());
 app.unsubscribe(bodyParser.json());
 
+//Connect application to mongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/todos',{ useNewParser: true})
+const dbConnection = mongoose.connection;
+
+dbConnection.once('open',()=>{
+    console.log("MongoDB database connection established successfully.");
+});
+
+//Start listening on port
 app.listen(PORT, () => {
     console.log("Server is running on Port: "+ PORT);
 })
