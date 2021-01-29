86cf9c19e065878730555f3643d04353f7f57903
wes-public-apps <murraywj@yahoo.com>
2021-01-29 08:10:06 -0800

    create routes for adding, editing, and loading todo items

--- backend/server.js
+++ backend/server.js
@@ -25,3 +25,65 @@ dbConnection.once('open',()=>{
 app.listen(PORT, () => {
     console.log("Server is running on Port: "+ PORT);
 })
+
+//Handle traffic routing
+const todoRoutes = express.Router();
+
+// retrieve all todos
+todoRoutes.route('/').get((req,res) => {
+    Todo.find( //I have concerns here that Todois not being found from model export
+        (err, todos)=>{
+            if(err){
+                console.log(err);
+            } else {
+                res.json(todos);
+            }
+        }
+    );
+});
+
+//retrieve a todo item with a specific id
+todoRoutes.route('/:id').get((req,res)=>{
+    let id = req.params.id;
+    Todo.findById(id,(err,todo)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.json(todo);
+        }
+    });
+});
+
+//add a todo item
+todoRoutes.route('/add').post((req,res)=>{
+    let todo = new Todo(req.body);
+    Todo.save()
+    .then(todo => {
+        res.status(200).json({'todo':'todo added successfully'});
+    })
+    .catch(err =>{
+        res.status(400).send("adding new todo failed");
+    });
+});
+
+//edit a todo item
+todoRoutes.route('/update/:id').post((req,res)=>{
+    Todo.findById(req.params.id,(err,todo)=>{
+        if(!todo) res.status(404).send("data is not found");
+        else {
+            todo.todo_description=req.body.todo_description;
+            todo.todo_responsible=req.body.todo_responsible;
+            todo.todo_priority=req.body.todo_priority;
+            todo.todo_completed=req.body.todo_completed;
+
+            todo.save().then(todo =>{
+                res.json("Todo updated!");
+            })
+            .catch(err =>{
+                res.status(400).send("Update failed");
+            });
+        }
+    });
+});
+
+app.use('/todos',todoRoutes);
